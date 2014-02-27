692b62b4b4cb68ddc2c19b4768158717f1fe42a5
Molez <hodgson.james.r@gmail.com>
2014-02-26 19:49:33 -0700

    Added script for populating persons table

---
+++ scripts/table_data.sql
@@ -0,0 +1,142 @@
+/* Load up the persons table */
+INSERT INTO persons
+(person_id, first_name, last_name, address, email, phone)
+VALUES
+(1, 'James', 'Hodgson', '4408 Pleasant Swale, Waterloo, Manitoba, R7N-3Y6, CA', 
+'UTeredly2489@armyspy.com', '(204) 159-5633');
+/*----------------------------------------------------------------------------*/
+INSERT INTO persons
+(person_id, first_name, last_name, address, email, phone)
+VALUES
+(2, 'Tyler', 'Wendlandt', '8838 Gentle By-pass, Frick, Saskatchewan, S4T-4M2, CA', 
+'USireares1966@dayrep.com', '(306) 564-1334');
+/*----------------------------------------------------------------------------*/
+INSERT INTO persons
+(person_id, first_name, last_name, address, email, phone)
+VALUES
+(3, 'Troy', 'Murphy', '5000 Foggy Road, Sucker Flat, Alberta, T6N-6U9, CA', 
+'HDiesse78@rhyta.com', '(403) 360-2027');
+/*----------------------------------------------------------------------------*/
+INSERT INTO persons
+(person_id, first_name, last_name, address, email, phone)
+VALUES
+(4, 'Bertha', 'Mcguire', '2106 Fallen Barn Park, Crapo, Newfoundland, A4R-6L0, CA', 
+'Trounertaing52@armyspy.com', '(709) 401-1921');
+/*----------------------------------------------------------------------------*/
+INSERT INTO persons
+(person_id, first_name, last_name, address, email, phone)
+VALUES
+(5, 'Damon', 'Thomas', '5527 Velvet Isle, Tessier, Manitoba, R8Q-4S4, CA', 
+'Dittleatized70@fleckens.hu', '(204) 577-0762');
+/*----------------------------------------------------------------------------*/
+INSERT INTO persons
+(person_id, first_name, last_name, address, email, phone)
+VALUES
+(6, 'Alex', 'Garcia', '5472 Shady Apple Ridge, Coronach, New Brunswick, E5X-2H9, CA', 
+'QRegan1959@dayrep.com', '(506) 479-0612');
+/*----------------------------------------------------------------------------*/
+INSERT INTO persons
+(person_id, first_name, last_name, address, email, phone)
+VALUES
+(7, 'Denise', 'Drake', '6023 Wishing Terrace, Odd, Prince Edward Island, C1R-1S3, CA', 
+'SUght1944@rhyta.com', '(902) 038-8934');
+/*----------------------------------------------------------------------------*/
+INSERT INTO persons
+(person_id, first_name, last_name, address, email, phone)
+VALUES
+(8, 'Stanley', 'Bush', '9639 Stony Diversion, Moosup Valley, Prince Edward Island, C5R-3A0, CA', 
+'LOftepts3903@rhyta.com', '(902) 264-0086');
+/*----------------------------------------------------------------------------*/
+INSERT INTO persons
+(person_id, first_name, last_name, address, email, phone)
+VALUES
+(9, 'Darla', 'Owens', '8137 High Port, Mililani, Alberta, T2T-8X4, CA', 
+'PExual1947@gustr.com', '(587) 157-8611');
+/*----------------------------------------------------------------------------*/
+INSERT INTO persons
+(person_id, first_name, last_name, address, email, phone)
+VALUES
+(10, 'Pedro', 'Walton', '5081 Old Rise Turnabout, McCool, Yukon, Y7Y-6J6, CA', 
+'Bege993@jourrapide.com', '(867) 428-2146');
+/*----------------------------------------------------------------------------*/
+INSERT INTO persons
+(person_id, first_name, last_name, address, email, phone)
+VALUES
+(11, 'Anna', 'Simpson', '6022 Thunder View Villas, Tuggleville, Manitoba, R6N-6U9, CA', 
+'Gled6991@teleworm.us', '(204) 252-0350');
+/*----------------------------------------------------------------------------*/
+INSERT INTO persons
+(person_id, first_name, last_name, address, email, phone)
+VALUES
+(12, 'Vincent', 'Foster', '9777 Rustic Anchor Nook, Coeur d\' Alene, Manitoba, R3N-4I8, CA', 
+'Oning1942@teleworm.us', '(204) 391-9837');
+/*----------------------------------------------------------------------------*/
+INSERT INTO persons
+(person_id, first_name, last_name, address, email, phone)
+VALUES
+(13, 'Bradley', 'Perez', '1868 Green Street, Purchase, Yukon, Y2S-7Q4, CA', 
+'HManoth78@armyspy.com', '(867) 321-2653');
+/*----------------------------------------------------------------------------*/
+INSERT INTO persons
+(person_id, first_name, last_name, address, email, phone)
+VALUES
+(14, 'Lois', 'Frazier', '1881 Bright Oak Boulevard, Two Rock, Britsh Columbia, V2O-4D0, CA', 
+'Cloy4853@armyspy.com', '(604) 941-9459');
+/*----------------------------------------------------------------------------*/
+INSERT INTO persons
+(person_id, first_name, last_name, address, email, phone)
+VALUES
+(15, 'Stacey', 'Malone', '4017 Merry Pond Rise, Little Water, Ontario, P8Q-4Z2, CA', 
+'VBaccupon3502@cuvox.de', '(613) 768-4763');
+/*----------------------------------------------------------------------------*/
+INSERT INTO persons
+(person_id, first_name, last_name, address, email, phone)
+VALUES
+(16, 'Patty', 'Lloyd', '2189 Hazy Spring Towers, Seven Mile, Newfoundland, A0C-4K1, CA', 
+'Xvier7325@superrito.com', '(709) 397-9074');
+/*----------------------------------------------------------------------------*/
+INSERT INTO persons
+(person_id, first_name, last_name, address, email, phone)
+VALUES
+(17, 'Paula', 'Young', '251 Dewy Lake Mount, Mankota, Ontario, M1U-3P6, CA', 
+'james@james.com', '(807) 199-1107');
+/*----------------------------------------------------------------------------*/
+INSERT INTO persons
+(person_id, first_name, last_name, address, email, phone)
+VALUES
+(18, 'Irving', 'Flores', '5769 Clear Island Run, Troublesome, Ontario, M0O-4Z6, CA', 
+'ZWharand2434@rhyta.com', '(519) 989-6438');
+/*----------------------------------------------------------------------------*/
+INSERT INTO persons
+(person_id, first_name, last_name, address, email, phone)
+VALUES
+(19, 'Lauren', 'Wise', '2807 Round Panda Lane, Earth, Nova Scotia, B3Q-9X3, CA', 
+'XAmmed7308@jourrapide.com', '902) 076-7477');
+/*----------------------------------------------------------------------------*/
+INSERT INTO persons
+(person_id, first_name, last_name, address, email, phone)
+VALUES
+(20, 'Peter', 'Griffith', '1742 Silver Cape, Mermaid, Prince Edward Island, C8Q-3F7, CA', 
+'LGayses1947@gustr.com', '(902) 757-1795');
+/*----------------------------------------------------------------------------*/
+INSERT INTO persons
+(person_id, first_name, last_name, address, email, phone)
+VALUES
+(21, 'Ian', 'Jimenez', '1742 Silver Cape, Mermaid, Prince Edward Island, C8Q-3F7, CA', 
+'Faies1934@dayrep.com', '(506) 478-7703');
+/*----------------------------------------------------------------------------*/
+INSERT INTO persons
+(person_id, first_name, last_name, address, email, phone)
+VALUES
+(22, 'Stephen', 'Becker', '9138 Honey Nectar Island, Calypso, Alberta, T9D-2H5, CA', 
+'Shaden8074@rhyta.com', '(587) 576-1175');
+/*----------------------------------------------------------------------------*/
+INSERT INTO persons
+(person_id, first_name, last_name, address, email, phone)
+VALUES
+(23, 'Anthony', 'Blake', '1207 Little Inlet, Concept, Quebec, H3F-9P6, CA', 
+'Forts9004@gustr.com', '(514) 195-4737');
+/*----------------------------------------------------------------------------*/
+
+/*Load up the users table*/
+
