503a73dbdbee0762bbd71d0f5cba89a994203858
twendlandt <twendlan@ualberta.ca>
2014-03-02 11:07:03 -0700

    worked on draft of login module, still not able to test

--- proj/LoginModule.php
+++ proj/LoginModule.php
@@ -1,5 +1,6 @@
 <html>
 <head>
+	<?php include("./inc/PHPconnectionDB.php'"); ?>
 	<title>Login Module - CMPUT 391 TTJ</title>
 	<!-- This is a work in progress I still need to 
 		Implement many features. 
@@ -8,14 +9,34 @@
 		- Let user swap their usernames and passwords -->
 </head>
 	<body>
+		<?php session_start(); ?>
 		<h1>Welcome to The Radiology Information System</h1>
 		<?php 
 			// first check if the post was set
 			if (isset($_POST['validate'])) {
-				$userName = $_POST['username'];
-				$password = $_POST['password'];
+				$_SESSION['username'] = $_POST['username'];
+				$_SESSSION['password'] = $_POST['password'];
 			
-				echo 'Thank you for logging in, ' . $userName . ".";
+				//HERE I SHOULD connect to the oracle db to check username/password
+				$conn = connect();
+				$sql = "SELECT * FROM users WHERE user_name = " . 
+				    $_SESSION['username'] . " AND password = " . 
+				    $_SESSION['password'] . ";";
+			
+				$stid = oci_parse($conn, $sql);
+				$res = oci_execute($stid);
+				
+				/*
+				 * Check if the user has entered the correct login info
+				 * if not deny them. 
+				 * NOTE: I'm not sure if this is exactly how you check but
+				 * I will confirm as soon as I am able to test :)
+				 */
+				if (!$res) {
+					echo "INVALID login information";  
+				} else {
+					echo 'Thank you for logging in, ' . $_SESSION['username'] . ".";
+				}
 			}
 		?>
 	</body>
