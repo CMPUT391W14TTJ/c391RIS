35a81ecaba2234dcedd26877c012897ba4e74f26
twendlandt <twendlan@ualberta.ca>
2014-03-10 16:15:53 -0600

    separated navigation/makefile is perfff

--- proj/account_settings.php
+++ proj/account_settings.php
@@ -5,17 +5,7 @@
 </head>
 <body>
 <div id="container">
-	<article class="navigation">
-		<nav>
-		    <li><a href="./home.php" title="Home">Home</a></li>
-		    <li><a href="./account_settings.php" title="Account Settings">Account Settings</a></li>
-		    <li><a href="./user_management.php" title="User Management">User Management</a></li>
-		    <li><a href="./report_generator.php" title="Report Generator">Report Generator</a></li>
-		    <li><a href="./upload.php" title="Uploading Module">Upload</a></li>
-		    <li><a href="./search.php" title="Search Module">Search</a></li>
-		    <li><a href="./data_analysis.php" title="Data Analysis">Data Analysis</a></li>
-		</nav>
-	</article>
+	<?php include('./inc/navigation.php'); ?>
 	<article class="acc_settings">
 		<h2>Account Settings</h2>
 		<div class="form">
--- proj/home.php
+++ proj/home.php
@@ -4,17 +4,7 @@
 	<?php session_start(); ?>
 </head>
 <body>
-	<article class="navigation">
-		<nav>
-		    <li><a href="./home.php" title="Home">Home</a></li>
-		    <li><a href="./account_settings.php" title="Account Settings">Account Settings</a></li>
-		    <li><a href="./user_management.php" title="User Management">User Management</a></li>
-		    <li><a href="./report_generator.php" title="Report Generator">Report Generator</a></li>
-		    <li><a href="./upload.php" title="Uploading Module">Upload</a></li>
-		    <li><a href="./search.php" title="Search Module">Search</a></li>
-		    <li><a href="./data_analysis.php" title="Data Analysis">Data Analysis</a></li>
-		</nav>
-	<article>
+	<?php include('./inc/navigation.php'); ?>
 	<article class="user_info">
 		<h1>User Info</h1>
 		<p>
