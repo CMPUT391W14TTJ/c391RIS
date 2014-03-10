5ff0b9482fadc60c682ef6b28f8681c63b7d68e5
twendlandt <twendlan@ualberta.ca>
2014-03-10 16:05:38 -0600

    fixed nav

--- proj/account_settings.php
+++ proj/account_settings.php
@@ -1,9 +1,10 @@
 <HTML>
 <head>
-	<title>Homepage - Radiology Information System</title>
+	<title>Account Settings - Radiology Information System</title>
 	<?php session_start(); ?>
 </head>
 <body>
+<div id="container">
 	<article class="navigation">
 		<nav>
 		    <li><a href="./home.php" title="Home">Home</a></li>
@@ -14,16 +15,19 @@
 		    <li><a href="./search.php" title="Search Module">Search</a></li>
 		    <li><a href="./data_analysis.php" title="Data Analysis">Data Analysis</a></li>
 		</nav>
-	<article>
-	<article class="user_info">
-		<h1>User Info</h1>
-		<p>
-		<?php
-			echo "USERNAME: " . $_SESSION['username'] . '</br>';
-			echo "PASSWORD: " . $_SESSION['password'] . '</br>';
-			echo "CLASS: " . $_SESSION['user_class'] . '</br>';
-		?>
-		</p>
 	</article>
+	<article class="acc_settings">
+		<h2>Account Settings</h2>
+		<div class="form">
+			<h3>Change your password:</h3>
+			<form name="pass_change" method="post" action="">
+				Old Password: <input type="password" name="oldPass"/><br/>
+				New Password: <input type="password" name="newPass1"/><br/>
+				Re-type New Password: <input type="password" name="newPass2"/><br/>
+				<input type="submit" name="PassChange" value="Change Password"/>
+			</form>
+		</div>
+	</article>		
+</div>
 </body>
 </html>
