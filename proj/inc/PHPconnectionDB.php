a28b01e045c72f7703153d5b0c5097819ec95c69
Molez <hodgson.james.r@gmail.com>
2014-03-17 17:01:40 -0600

    Changed to james' database

--- proj/inc/PHPconnectionDB.php
+++ proj/inc/PHPconnectionDB.php
@@ -1,6 +1,6 @@
 <?php
 function connect(){
-	$conn = oci_connect('twendlan', 'cmput391ttj');
+	$conn = oci_connect('jhodgson', 'qwer1234');
 	if (!$conn) {
 		$e = oci_error();
 		trigger_error(htmlentities($e['message'], ENT_QUOTES), E_USER_ERROR);
--- scripts/inverted_index.sql
+++ scripts/inverted_index.sql
@@ -1,29 +1,14 @@
 CREATE INDEX rad_rec_diagnosis on radiology_record(diagnosis)
-INDEXTYPE IS CTXSYS.CONTEXT
-/*parameters ('DATASTORE CTXSYS.DEFAULT_DATASTORE');*/
+INDEXTYPE IS CTXSYS.CONTEXT;
 
 CREATE INDEX rad_rec_description on radiology_record(description)
-INDEXTYPE IS CTXSYS.CONTEXT
-/*parameters ('DATASTORE CTXSYS.DEFAULT_DATASTORE');*/
+INDEXTYPE IS CTXSYS.CONTEXT;
 
 CREATE INDEX rad_rec_test_type on radiology_record(test_type)
-INDEXTYPE IS CTXSYS.CONTEXT
-/*parameters ('DATASTORE CTXSYS.DEFAULT_DATASTORE');*/
+INDEXTYPE IS CTXSYS.CONTEXT;
 
 CREATE INDEX person_first_name on persons(first_name)
-INDEXTYPE is CTXSYS.CONTEXT
-/*parameters ('DATASTORE CTXSYS.DEFAULT_DATASTORE');*/
+INDEXTYPE is CTXSYS.CONTEXT;
 
 CREATE INDEX person_last_name on persons(last_name)
-INDEXTYPE is CTXSYS.CONTEXT
-/*parameters ('DATASTORE CTXSYS.DEFAULT_DATASTORE');*/
-/*
-CREATE INDEX sv_diagnosis on searchView(diagnosis)
-INDEXTYPE IS CTXSYS.CONTEXT;
-
-CREATE INDEX sv_description on searchView(description)
-INDEXTYPE IS CTXSYS.CONTEXT;
-
-CREATE INDEX sv_name on searchView(patient_name)
 INDEXTYPE is CTXSYS.CONTEXT;
-*/
