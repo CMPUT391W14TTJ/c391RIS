8703554b42e72afd3675c2b29e4d84331a510b81
twendlandt <twendlan@ualberta.ca>
2014-03-28 15:25:45 -0600

    poosh 2

--- proj/account_settings.php
+++ proj/account_settings.php
@@ -7,7 +7,7 @@
 	<?php 
 		include('./inc/getInfo.php'); 
 		session_start();
-		if(!isset($_SESSION['user']->username)) {
+		if(!isset($_SESSION['user']->username) || $_SESSION['user'] == null) {
 			header('Location: ./index.html');
 		}
 		include('./inc/navigation.php'); 
